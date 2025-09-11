b35cb4d65f3c901962e7c312d90274cb18506c35
annora.vandanu@ui.ac.id <vandanu@Vandanu.localdomain>
2025-09-11 09:31:25 +0700

    Update the matlab geometry scripts

--- Scripts/1_geometry/matlab-octave/geom_octave.m
+++ Scripts/1_geometry/matlab-octave/geom_octave.m
@@ -1,25 +1,27 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-clear; clc
+close all; clear all; clc;
+
 load SPS_extract.txt
 load RPS_extract.txt
 
-% Ingat no of shots: 251,no of receiver in each shot: 282,total number of traces is 251*282=70782, jumlah trace tersebut setelah kill trace -1 - 0
+% From the data header, no of shot: 251, no of receiver in each shot: 282, total number of traces is 251*282=70782. Total trace is only from seismic data without aux.
+% Input Parameter
+no_of_shot = 251;
+no_of_receiver = 282;
 
-%%%menyusun matrix sps untuk seluruh trace (70782)%%%%%%
-for i=1:251
-sps_for_traces_in_each_shot{i}=repmat(SPS_extract(i,:),282,1);
+% ----------------------- Expanding SPS for all traces ----------------------
+for i=1:no_of_shot
+sps_for_traces_in_each_shot{i}=repmat(SPS_extract(i,:),no_of_receiver,1);
 end
 sps_all_traces=cell2mat(sps_for_traces_in_each_shot');
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%%%menyusun matrix rps untuk seluruh trace (70782)%%%%%%
-for i=1:251
-rps_for_traces_in_each_shot{i}=RPS_extract([(i*2)-1:281+(i*2)-1]',:);
+% ----------------------- Expanding RPS for all traces ----------------------
+for i=1:no_of_shot
+rps_for_traces_in_each_shot{i}=RPS_extract([(i*2)-1:(no_of_receiver-1)+(i*2)-1]',:);
 end
 rps_all_traces=cell2mat(rps_for_traces_in_each_shot');
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%%%% menghitung offset
+% ----------------------- Calculating offset ----------------------
 sx=sps_all_traces(:,2);
 sy=sps_all_traces(:,3);
 selev=sps_all_traces(:,4);
@@ -32,24 +34,17 @@ gstat=rps_all_traces(:,5);
 ox=gx-sx;
 oy=gy-sy;
 offset=sqrt(ox.^2+oy.^2);
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%%%menghitung cdp untuk masing-masing trace
-%%%dikarenakan interval geophone 25m dan interval sumber 50m, maka:
-%%%cdp untuk shot pertama adalah 1 s/d 282
-%%%cdp untuk shot kedua adalah 5 s/d 286
-%%%cdp untuk shot ketiga adalah 9 s/d 290 dst....(lihat ilustrasi stacking diagram dibawah untuk memahaminya)
-for i=1:251
+% ----------------------- Assigning CDP Number ----------------------
+for i=1:no_of_shot
 cdp_each_shot{i}=[(4*i-3):281+(4*i-3)]';
 end
 cdp_all_traces=cell2mat(cdp_each_shot');
 
+% ----------------------- Saving header matrix ----------------------
 geom_header=[sx,sy,selev,sstat,gx,gy,gelev,gstat,cdp_all_traces,offset];
-
 save -ascii geom_header.txt geom_header
 
-
-%%%%plot koordinat sumber dan penerima
+% ----------------------- Plot of source-receiver ----------------------
 plot(sx,sy,'r*'); hold on
-plot(gx,gy,'b^');xlabel('Easting');ylabel('Northing');legend('Source','Geophone');title("Source Receiver Plot")
-%%%%akhir dari kode
+plot(gx,gy,'b^');xlabel('Easting');ylabel('Northing');legend('Source','Geophone');title('Source Receiver Plot'); grid on
--- Scripts/1_geometry/matlab-octave/regoff.m
+++ Scripts/1_geometry/matlab-octave/regoff.m
@@ -1,12 +1,38 @@
-clear; clc
-%%%offset regularization...
+close all; clear all; clc
+% ----------------------- Offset Regularization ----------------------
 right=[25:25:3525]';
 left=[-3525:25:-25]';
 offset_each_shot=[left;right];
 offset_all_shot=repmat(offset_each_shot,251,1);
 
-%%%mengganti offset pada geom_header.txt dengan offset yang baru
+% ----------------------- Modify the Offset ----------------------
 load geom_header.txt
 geom_header_reg_offset=[geom_header(:,[1:9]),offset_all_shot];
 save -ascii geom_header_reg_offset.txt geom_header_reg_offset
-%%%%%
+
+% % ----------------------- Show the Plot ----------------------
+offset = geom_header(:,10); 
+cdp = geom_header(:,9);
+
+sx = geom_header_reg_offset(:,1);
+sy = geom_header_reg_offset(:,2);
+gx = geom_header_reg_offset(:,5);
+gy = geom_header_reg_offset(:,6);
+offset_reg = geom_header_reg_offset(:,10); 
+cdp_reg = geom_header_reg_offset(:,9);
+
+cmp_x = (geom_header_reg_offset(:,1)+geom_header_reg_offset(:,5))/2;
+cmp_y = (geom_header_reg_offset(:,2)+geom_header_reg_offset(:,6))/2;
+
+figure;plot(cdp,offset);title('Stacking Diagram Before Regularization');xlabel('CDP');ylabel('Offset')
+figure;plot(cdp_reg,offset_reg);title('Stacking Diagram After Regularization');xlabel('CDP');ylabel('Offset')
+
+figure;
+plot(sx,sy,'r*'); hold on
+plot(gx,gy,'b^');
+plot(cmp_x,cmp_y,'g+');
+xlabel('Easting');
+ylabel('Northing');
+legend('Source','Geophone','CMP Location','Location','NorthWest');
+title('Source-Receiver Plot and CMP Locations');
+grid on
